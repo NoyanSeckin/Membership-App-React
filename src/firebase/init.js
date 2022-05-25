fa7d61e8198d265f1308be10ad1ac912dfa2c083
FKNoyan <egemennoyan@hotmail.com>
2022-05-25 17:15:32 +0300

    initial setup

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAtWQRLj5GLu6F0e9ex-DMhLz2HBFS1SA",
+  authDomain: "steel-membership-app.firebaseapp.com",
+  projectId: "steel-membership-app",
+  storageBucket: "steel-membership-app.appspot.com",
+  messagingSenderId: "158745713285",
+  appId: "1:158745713285:web:19150944933aefc87ea7ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
